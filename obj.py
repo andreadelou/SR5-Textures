20c8c1381ac2f1f9680976015cd0538c0d76a8ed
Andrea Lam <70552370+andreadelou@users.noreply.github.com>
2022-11-28 17:37:55 -0600

    Done

--- obj.py
+++ obj.py
@@ -1,17 +1,16 @@
-import struct 
+import struct
 
-def color(r, g, b):
-  return bytes([b, g, r])
-
+def color_select(r, g, b):
+   return bytes([b, g, r])
 
 class Obj(object):
     def __init__(self, filename):
         with open(filename) as f:
             self.lines = f.read().splitlines()
-            self.vertices = []
-            self.tvertices = []
-            self.vfaces = []
-            self.read()
+        self.vertex = []
+        self.tvertex = []
+        self.faces = []
+        self.read()
 
     def read(self):
         for line in self.lines:
@@ -21,52 +20,54 @@ class Obj(object):
                 except:
                     prefix = ''
                 if prefix == 'v':
-                    self.vertices.append(list(map(float, value.split(' '))))
+                    self.vertex.append(list(map(float, value.split(' '))))
                 if prefix == 'vt':
-                    self.tvertices.append(list(map(float, value.split(' '))))                    
+                    self.tvertex.append(list(map(float, value.split(' '))))                    
                 elif prefix == 'f':
                     try:
-                        self.vfaces.append([list(map(int , face.split('/'))) for face in value.split(' ')])
+                        self.faces.append([list(map(int , face.split('/'))) for face in value.split(' ')])
                     except:
-                        self.vfaces.append([list(map(int , face.split('//'))) for face in value.split(' ')])
-
+                        self.faces.append([list(map(int , face.split('//'))) for face in value.split(' ')])
 
 class Texture(object):
+    
     def __init__(self, path):
         self.path = path
         self.read()
-
+    
     def read(self):
-        image = open(self.path, "rb")
-        image.seek(2 + 4 + 4) 
-        header_size = struct.unpack("=l", image.read(4))[0]  
-        image.seek(2 + 4 + 4 + 4 + 4)
+        with open(self.path, 'rb') as image:
+            image.seek(10)
+            header_size = struct.unpack('=l', image.read(4))[0]
+            image.seek(18)
+            self.width = struct.unpack('=l', image.read(4))[0]
+            self.height = struct.unpack('=l', image.read(4))[0]
+            
+            image.seek(header_size)
+            
+            self.pixels = []
+            
+            for y in range(self.height):
+                self.pixels.append([])
+                for x in range(self.width):
+                    b = ord(image.read(1))
+                    g = ord(image.read(1))
+                    r = ord(image.read(1))
+                    
+                    self.pixels[y].append(
+                        color_select(r, g, b)
+                    )
+                    
+    def get_color(self, tx, ty):
+        x = round(tx * self.width)
+        y = round(ty * self.height)
         
-        self.width = struct.unpack("=l", image.read(4))[0]  
-        self.height = struct.unpack("=l", image.read(4))[0] 
-        self.pixels = []
-        image.seek(header_size)
-        for y in range(self.height):
-            self.pixels.append([])
-            for x in range(self.width):
-                b = ord(image.read(1))
-                g = ord(image.read(1))
-                r = ord(image.read(1))
-                self.pixels[y].append(color(r,g,b))
-        image.close()
-
-    def get_color(self, tx, ty, intensity=1):
-        x = int(tx * self.width)
-        y = int(ty * self.height)
-        try:
-            return bytes(map(lambda b: round(b*intensity) if b*intensity > 0 else 0, self.pixels[y][x]))
-        except:
-            pass  
+        return self.pixels[y][x]
     
     def get_color_with_intensity(self, tx, ty, intensity):
         x = round(tx * self.width)
         y = round(ty * self.height)
-
+        
         try:
             return bytes(map(lambda b: round(b*intensity) 
                 if b*intensity > 0 
